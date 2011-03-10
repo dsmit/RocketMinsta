b5fc661f8a53a766d32963027e1b18c308cb748b
Akari <hetors.email@gmail.com>
2011-03-10 12:41:32 +0200

    A release system

---
+++ EXAMPLE_releaseconfig.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+# Where to store released packages.
+RELEASE_PKGPATH="release"
+
+# Will be appended to RM_BUILD_NAME. Can be left blank.
+RELEASE_SUFFIX=""
+
+# Will be appended to RM_BUILD_DATE. Should not be left blank, may be left unchanged.
+RELEASE_DESCRIPTION="release build"
+
+# Whether to include the rm-custom directory or not. 1 = include, 0 = don't include.
+RELEASE_RMCUSTOM=1
+
+# Name of a rm-custom configuration to be made default. May be left blank.
+# Note: only the config NAME has to be specified.
+# Example: to make rm-custom/akari.cfg the default config, you have to specify "akari"
+# Another note: changing this will also affect RELEASE_SUFFIX
+RELEASE_DEFAULTCFG=""
+
+# If 1, the release directory will be wiped out after package generation
+RELEASE_CLEANUP=1
+
+# This function will be called when the release is ready but hasn't been packaged yet,
+# for you to customize the release here. May be left as-is.
+#
+# First argument: full path to the release directory
+
+function prepackage
+{
+    
+}
+
+# This function will be called after the release has been packaged. May be left as-is.
+#
+# First argument: full path to the release package
+
+function postpackage
+{
+    
+}
--- build.sh
+++ build.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 
+RELEASE=0
+BUILD_DATE="$(date +"%F %T %Z")"
+BUILD_DATE_PLAIN="$(date +%y%m%d%H%M%S)"
+BRANCH="`git symbolic-ref HEAD 2>/dev/null | sed -e 's@^refs/heads/@@'`"
+
 function error
 {
     echo -e "\n$*" >&2
     exit 1
 }
-
-[ -e "config.sh" ] || error "No configuration file found. Please run \`cp EXAMPLE_config.sh config.sh', edit config.sh and try again."
-. "config.sh" || error "Failed to read configuration"
 
 function getqcc
 {
@@ -32,9 +34,6 @@ function getqcc
     echo "$qcc"
 }
 
-[ -e "rocketminsta.cfg" ] || error "rocketminsta.cfg wasn't found"
-USEQCC="$(getqcc)"
-
 function buildqc
 {
     qcdir="$QCSOURCE/$1"
@@ -45,22 +44,24 @@ function buildqc
     popd &>/dev/null
 }
 
-BRANCH="`git symbolic-ref HEAD 2>/dev/null | sed -e 's@^refs/heads/@@'`"
+function buildall
+{
+    # $1 = suffix
+    # $2 = desc
+    
+    USEQCC="$(getqcc)"
 
-echo "#define RM_BUILD_DATE \"$(date +"%F %T %Z") (git build)\""          >  "$QCSOURCE"/common/rm_auto.qh
-echo "#define RM_BUILD_NAME \"RocketMinsta-$BRANCH\"" >> "$QCSOURCE"/common/rm_auto.qh
+    echo "#define RM_BUILD_DATE \"$BUILD_DATE ($2)\""          >  "$QCSOURCE"/common/rm_auto.qh
+    echo "#define RM_BUILD_NAME \"RocketMinsta$1\"" >> "$QCSOURCE"/common/rm_auto.qh
 
-buildqc server/
-mv -v progs.dat "$SVPROGS"
+    buildqc server/
+    mv -v progs.dat "$SVPROGS"
 
-buildqc client/
-mv -v csprogs.dat "$CSPROGS"
+    buildqc client/
+    mv -v csprogs.dat "$CSPROGS"
 
-rm -v "$QCSOURCE"/common/rm_auto.qh
-
-cp -v "rocketminsta.cfg" "$NEXDATA"
-mkdir -pv "$NEXDATA/rm-custom"
-cp -v rm-custom/* "$NEXDATA/rm-custom"
+    rm -v "$QCSOURCE"/common/rm_auto.qh
+}
 
 function listcustom()
 {
@@ -68,6 +69,116 @@ function listcustom()
         echo -e "\t\t$cfg : $(head -1 "$cfg" | sed -e 's@//cfgname:@@')"
     done
 }
+
+################################################################################
+
+[ -e "config.sh" ] || error "No configuration file found. Please run \`cp EXAMPLE_config.sh config.sh', edit config.sh and try again."
+. "config.sh" || error "Failed to read configuration"
+
+if [ "$1" = "release" ]; then
+    RELEASE=1
+    [ -e "releaseconfig.sh" ] || error "No release configuration file found. Please run \`cp EXAMPLE_releaseconfig.sh releaseconfig.sh', edit releaseconfig.sh and try again."
+    . "releaseconfig.sh" || error "Failed to read release configuration"
+    
+    [ -n "$RELEASE_SUFFIX"     ] && RELEASE_REALSUFFIX="-$RELEASE_SUFFIX"
+    [ z"$BRANCH" = z"master"   ] || RELEASE_REALSUFFIX="-$BRANCH$RELEASE_REALSUFFIX"
+    
+    if [ -n "$RELEASE_DEFAULTCFG" ]; then
+        if [ -n "$RELEASE_REALSUFFIX" ]; then
+            RELEASE_REALSUFFIX="${RELEASE_REALSUFFIX}_cfg$RELEASE_DEFAULTCFG"
+        else
+            RELEASE_REALSUFFIX="-cfg$RELEASE_DEFAULTCFG"
+        fi
+        
+        [ -e "rm-custom/$RELEASE_DEFAULTCFG.cfg" ] || error "Default configuration '$RELEASE_DEFAULTCFG.cfg' does not exist in rm-custom"
+    fi
+    
+    PKGNAME="RocketMinsta${RELEASE_REALSUFFIX}"
+    RELEASE_PKGNAME="${PKGNAME}_$BUILD_DATE_PLAIN"
+    RELEASE_PKGPATH="$(readlink -f "$RELEASE_PKGPATH")"
+    mkdir "$RELEASE_PKGPATH/$RELEASE_PKGNAME" || error "Failed to create package directory"
+
+    NEXDATA="$(readlink -f "$RELEASE_PKGPATH/$RELEASE_PKGNAME")"
+    SVPROGS="$NEXDATA/sv_mod.dat"
+    CSPROGS="$NEXDATA/cl_mod.dat"
+    
+    buildall $RELEASE_REALSUFFIX "$RELEASE_DESCRIPTION"
+    
+    cp -v "rocketminsta.cfg" "$NEXDATA"
+    if [ $RELEASE_RMCUSTOM -eq 1 ]; then
+        mkdir -pv "$NEXDATA/rm-custom"
+        cp -v rm-custom/* "$NEXDATA/rm-custom"
+    fi
+
+    if [ -n "$RELEASE_DEFAULTCFG" ]; then
+        cat "rm-custom/$RELEASE_DEFAULTCFG.cfg" >> "$NEXDATA/rocketminsta.cfg"
+        sed -i "/exec rocketminsta.cfg/d" "$NEXDATA/rocketminsta.cfg" # Without this, a recursive include will occur
+    fi
+    
+    cat <<EOF > "$NEXDATA/README.rmrelease"
+
+This is an auto generated $PKGNAME release package, built at $BUILD_DATE. Installation:
+    
+    1) Extract the contents of this package into your Nexuiz data directory (typically ~/.nexuiz/data/)
+    2) Edit your server config and add the following lines at very top:
+        
+        exec rocketminsta.cfg
+        set sv_progs $(echo "$SVPROGS" | sed -e 's@.*/@@g')
+        set csqc_progname $(echo "$CSPROGS" | sed -e 's@.*/@@g')
+EOF
+
+    if [ $RELEASE_RMCUSTOM -eq 1 ]; then
+        cat <<EOF >> "$NEXDATA/README.rmrelease"
+        
+        If you'd like to use one of the custom configurations,
+        add the following at the bottom of your config:
+        
+            exec rm-custom/NAME_OF_CUSTOM_CONFIG.cfg
+        
+        The following configurations were included at build time: `ls rm-custom/*.cfg | while read line; do line=${line##rm-custom/}; echo -n "$line "; done`
+EOF
+    fi
+
+    cat <<EOF >> "$NEXDATA/README.rmrelease"
+    3) Start the server and enjoy.
+
+RocketMinsta project: https://github.com/nexAkari/RocketMinsta
+
+EOF
+
+    prepackage "$RELEASE_PKGPATH/$RELEASE_PKGNAME" || error "prepackage failed"
+
+    pushd "$NEXDATA" &>/dev/null
+    tar -zcvf "$RELEASE_PKGPATH/$RELEASE_PKGNAME.tar.gz" * | while read line; do
+        echo "Adding file: $line"
+    done
+    popd &>/dev/null
+
+    if [ $RELEASE_CLEANUP -eq 1 ]; then
+        rm -vrf "$RELEASE_PKGPATH/$RELEASE_PKGNAME"
+    fi
+
+    postpackage "$RELEASE_PKGPATH/$RELEASE_PKGNAME.tar.gz" || error "postpackage failed"
+
+    cat << EOF
+**************************************************
+
+    Finished $PKGNAME release
+    
+    Package path:
+        $RELEASE_PKGPATH/$RELEASE_PKGNAME.tar.gz
+
+**************************************************
+EOF
+
+    exit
+fi
+
+buildall $BRANCH "git build"
+
+cp -v "rocketminsta.cfg" "$NEXDATA"
+mkdir -pv "$NEXDATA/rm-custom"
+cp -v rm-custom/* "$NEXDATA/rm-custom"
 
 cat <<EOF
 **************************************************
