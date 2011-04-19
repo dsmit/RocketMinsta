9622ce4b63de7c61535145d937910abb4b6ecb0e
Akari <hetors.email@gmail.com>
2011-04-19 11:08:21 +0300

    update.sh now also updates the xonotic repo

--- update.sh
+++ update.sh
@@ -14,12 +14,42 @@ function finish
 [ -e "updconfig.sh" ] || error "No configuration file found. Please run \`cp EXAMPLE_updconfig.sh updconfig.sh', edit updconfig.sh and try again."
 . "updconfig.sh" || error "Failed to read configuration"
 
+[ -e "config.sh" ] || error "No build configuration file found. Please run \`cp EXAMPLE_config.sh config.sh', edit config.sh and try again."
+. "config.sh" || error "Failed to read build configuration"
+
+. xonotic-conf.sh || error "Failed to read xonotic-conf"
+
 BRANCH="`git symbolic-ref HEAD 2>/dev/null | sed -e 's@^refs/heads/@@'`"
 
 if [ "$1" != "rebuild" ]; then
+    upd=0
+
     echo " -- Updating $BRANCH"
     git fetch || error "git fetch failed"
-    if [ "$(git merge origin/$BRANCH || error "git merge failed")" = "Already up-to-date." ] && [ "$1" != "force" ]; then
+    if [ "$(git merge origin/$BRANCH || error "git merge failed")" = "Already up-to-date." ]; then
+        echo "$BRANCH is already up to date."
+        let upd++
+    fi
+    
+    echo " -- Updating $XONDATA_RM_BRANCH"
+    
+    if [ "$(readlink -f "$QCSOURCE")" = "$(readlink -f "qcsrc")" ]; then
+        echo "QCSOURCE points to $(readlink -f "qcsrc"), correcting to $(readlink -f "xonotic-git/qcsrc")"
+        QCSOURCE="$(readlink -f "xonotic-git/qcsrc")"
+        
+        [ -e "$QCSOURCE" ] || error "QCSOURCE doesn't exist, WTF?!"
+    fi
+    
+    pushd "$QCSOURCE" &> /dev/null || error "cd failed (?!)"
+    git checkout "$XONDATA_RM_BRANCH" || error "git checkout failed"
+    
+    if [ "$(git pull || error "git pull failed")" = "Already up-to-date." ]; then
+        echo "$XONDATA_RM_BRANCH is already up to date."
+        let upd++
+    fi
+    popd &> /dev/null
+    
+    if [ $upd -eq 2 ] && [ "$1" != "force" ]; then
         echo "Already up to date, exiting."
         exit 0
     fi
